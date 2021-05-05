ae583791a94e03ca27171669d0403e4e8810869f
Dorukyum <doruk.ak@hotmail.com>
2021-05-05 14:56:58 +0300

    Make chatbot api key necessary

--- discord/ext/helpers/chatbot.py
+++ discord/ext/helpers/chatbot.py
@@ -1,14 +1,6 @@
-import aiohttp
+from prsaw import RandomStuff
 
 
-async def chatbot(message: str, *, api_key: str = None, language: str = "en") -> str:
-    message = "+".join(message.split())
-    url = (
-        f"https://api.pgamerx.com/ai/response?api_key={api_key}&"
-        if api_key
-        else "https://api.pgamerx.com/demo/ai/response?"
-    )
-    url += f"message={message}&language={language}"
-    async with aiohttp.ClientSession() as ses:
-        async with ses.get(url) as res:
-            return (await res.json())[0]
+async def chatbot(message: str, *, api_key: str, language: str = "en") -> str:
+    wrapper = RandomStuff(async_mode=True, api_key=api_key)
+    return await wrapper.get_ai_response(message, lang=language)
--- discord/ext/helpers/requests.py
+++ discord/ext/helpers/requests.py
@@ -6,16 +6,10 @@ class Requests:
     async def get(url, **kwargs):
         async with aiohttp.ClientSession() as ses:
             async with ses.get(url, **kwargs) as resp:
-                if hasattr(resp, "json"):
-                    return await resp.json()
-                else:
-                    return resp
+                return resp
 
     @staticmethod
     async def post(url, **kwargs):
         async with aiohttp.ClientSession() as ses:
             async with ses.post(url, **kwargs) as resp:
-                if hasattr(resp, "json"):
-                    return await resp.json()
-                else:
-                    return resp
+                return resp
