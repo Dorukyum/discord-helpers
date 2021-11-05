a036f2eed3b4b0c6c3f490f9745044e85b5a9417
Dorukyum <doruk.ak@hotmail.com>
2021-11-05 21:55:32 +0300

    Make chatbot accept session

--- discord/ext/helpers/chatbot.py
+++ discord/ext/helpers/chatbot.py
@@ -1,10 +1,17 @@
-import aiohttp
+from typing import Optional
+from aiohttp import ClientSession
 
 
-async def chatbot(message: str, *, api_key: str, language: str = "en") -> str:
+async def chatbot(
+    message: str,
+    *,
+    api_key: str,
+    language: str = "en",
+    session: Optional[ClientSession] = None
+) -> str:
     headers = {"x-api-key": api_key}
-    params = {"type": "stable", "message": message}
-    async with aiohttp.ClientSession() as ses:
+    params = {"type": "stable", "message": message, "language": language}
+    async with (session or ClientSession()) as ses:
         async with ses.get(
             "https://api.pgamerx.com/v3/ai/response", headers=headers, params=params
         ) as response:
