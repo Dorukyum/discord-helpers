e7f02dc4750ea4ae616536e863230cd374937c1a
Dorukyum <doruk.ak@hotmail.com>
2022-03-24 19:34:53 +0300

    Update webhooks.py

--- discord/ext/helpers/webhooks.py
+++ discord/ext/helpers/webhooks.py
@@ -2,8 +2,9 @@ import discord
 
 
 class Webhooks:
-    @staticmethod
+    @classmethod
     async def find_and_send(
+        cls,
         text: str,
         *,
         channel: discord.TextChannel,
@@ -14,18 +15,15 @@ class Webhooks:
         webhooks = await channel.webhooks()
         for webhook in webhooks:
             if webhook.name == webhook_name:
-                if not icon_url:
-                    icon_url = webhook.avatar
-                if not sender_name:
-                    sender_name = webhook.name
-                return await webhook.send(
-                    text, username=sender_name, avatar_url=icon_url
+                return await cls.send_message(
+                    text, webhook, sender_name=sender_name, icon_url=icon_url
                 )
         else:
             raise RuntimeError("No webhook was found named {}".format(webhook_name))
 
-    @staticmethod
+    @classmethod
     async def send_message(
+        cls,
         text: str,
         webhook: discord.Webhook,
         *,
